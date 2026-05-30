5213721232d8d4ddb1911304cd2019917a1e3258
Srayan Jana <valorzard.kiko@outlook.com>
2026-05-29 23:15:05 -0700

    got native working

--- client/src/connection.rs
+++ client/src/connection.rs
@@ -1,4 +1,3 @@
-#[cfg(target_arch = "wasm32")]
 use futures_util::{SinkExt, StreamExt};
 #[cfg(target_arch = "wasm32")]
 use gloo_timers::future::TimeoutFuture;
@@ -66,73 +65,62 @@ pub async fn connect_to_websocket_server_wasm() {
 
 #[cfg(not(target_arch = "wasm32"))]
 pub fn connect_to_websocket_server_native() {
-    use std::sync::{Arc, Mutex};
-    use tungstenite::{Message, connect};
-    let (socket, response) = connect(SERVER_ADDRESS).expect("Can't connect");
+    use tokio_tungstenite::{connect_async, tungstenite::Message};
 
-    println!("Connected to the server");
-    println!("Response HTTP code: {}", response.status());
-    println!("Response contains the following headers:");
-    for (header, _value) in response.headers() {
-        println!("* {header}");
-    }
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async {
+            let (socket, response) = connect_async(SERVER_ADDRESS)
+                .await
+                .expect("Can't connect");
 
-    let socket = Arc::new(Mutex::new(socket));
-    let send_socket = Arc::clone(&socket);
+            println!("Connected to the server");
+            println!("Response HTTP code: {}", response.status());
+            println!("Response contains the following headers:");
+            for (header, _value) in response.headers() {
+                println!("* {header}");
+            }
 
-    let send_loop = std::thread::spawn(move || {
-        let mut i = 0;
-        loop {
-            let message_to_send =
-                rpc::RpcMessage::Text(format!("Hello WebSocket Native {i}").to_string());
-            let bytes = rkyv::to_bytes::<rkyv::rancor::Error>(&message_to_send).unwrap();
-            let mut socket = match send_socket.lock() {
-                Ok(socket) => socket,
-                Err(e) => {
-                    println!("Error! Breaking send loop (mutex poisoned): {e}");
-                    break;
+            let (mut send_stream, mut recv_stream) = socket.split();
+
+            let send_loop = tokio::spawn(async move {
+                let mut i = 0;
+                loop {
+                    let message_to_send =
+                        rpc::RpcMessage::Text(format!("Hello WebSocket Native {i}").to_string());
+                    let bytes = rkyv::to_bytes::<rkyv::rancor::Error>(&message_to_send).unwrap();
+                    if let Err(e) = send_stream.send(Message::Binary(bytes.to_vec().into())).await {
+                        println!("Error! Breaking send loop: {e}");
+                        break;
+                    }
+                    i += 1;
+                    tokio::time::sleep(Duration::from_millis(1000)).await;
                 }
-            };
+            });
 
-            if let Err(e) = socket.send(Message::Binary(bytes.to_vec().into())) {
-                println!("Error! Breaking send loop: {e}");
-                break;
-            }
-
-            drop(socket);
-            i += 1;
-            std::thread::sleep(Duration::from_millis(1000));
-        }
-    });
-
-    loop {
-        let msg = {
-            match socket.lock() {
-                Ok(mut socket) => socket.read(),
-                Err(e) => {
-                    println!("Error! Breaking receive loop (mutex poisoned): {e}");
-                    break;
+            let recv_loop = tokio::spawn(async move {
+                while let Some(msg) = recv_stream.next().await {
+                    match msg {
+                        Ok(Message::Text(msg)) => {
+                            println!("Received text: {:?}", msg);
+                        }
+                        Ok(Message::Binary(msg)) => {
+                            let deserialized = rpc::decode_message(msg.as_ref()).unwrap();
+                            println!("Received binary: {:?}", deserialized);
+                        }
+                        Ok(msg) => {
+                            println!("Unexpected message: {:?}", msg);
+                        }
+                        Err(e) => {
+                            println!("Error! Breaking receive loop: {e}");
+                            break;
+                        }
+                    }
                 }
-            }
-        };
+            });
 
-        match msg {
-            Ok(Message::Text(msg)) => {
-                println!("Received text: {:?}", msg);
-            }
-            Ok(Message::Binary(msg)) => {
-                let deserialized = rpc::decode_message(msg.as_ref()).unwrap();
-                println!("Received binary: {:?}", deserialized);
-            }
-            Ok(msg) => {
-                println!("Unexpected message: {:?}", msg);
-            }
-            Err(e) => {
-                println!("Error! Breaking receive loop: {e}");
-                break;
-            }
-        }
-    }
-
-    let _ = send_loop.join();
+            let _ = tokio::join!(send_loop, recv_loop);
+        })
 }
