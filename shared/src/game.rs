3da93a6e9d882d57f33742b65a24b2202a2c5930
Srayan Jana <valorzard.kiko@outlook.com>
2026-07-13 23:22:51 -0700

    we don't need to copy all of this

--- shared/src/game.rs
+++ shared/src/game.rs
@@ -428,12 +428,11 @@ impl GameLogic {
         world.spawn((side, body_handle, collider_handle, controller, player_rect))
     }
 
-    pub fn get_obstacle_rectangles(&mut self) -> Vec<GameRectangle> {
+    pub fn get_obstacle_rectangles(&mut self) -> Vec<&GameRectangle> {
         self.world
             .query_mut::<(&GameRectangle, &Obstacle)>()
             .into_iter()
             .map(|(rectangle, _)| rectangle)
-            .copied()
             .collect()
     }
 
